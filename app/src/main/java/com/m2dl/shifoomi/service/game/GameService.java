ab46f8d522603f736a0c6ada121c5c956bad42cf
Arthur <bellal.arthur@gmail.com>
2017-03-17 16:03:45 +0100

    Minor fix

--- app/src/main/java/com/m2dl/shifoomi/service/game/GameService.java
+++ app/src/main/java/com/m2dl/shifoomi/service/game/GameService.java
@@ -18,7 +18,7 @@ public class GameService {
         GameRepositoryFirebase.getInstance().addGameRepositoryListener(userId, new GameRepositoryListener() {
             @Override
             public void gameUpdate(Game game) {
-                if (game.getGameMoves().isEmpty()) {
+                if (!game.getGameMoves().isEmpty()) {
                     gameListener.scoreUpdated(computeScore(userId, game));
                     GameMove newMove = game.getGameMoves().get(game.getGameMoves().size() - 1);
                     if (!newMove.getUserId().equals(userId))
