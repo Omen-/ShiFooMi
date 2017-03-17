e3d15a6cdff929cda40d064081e7f220a8ff369f
Alexandre Lagane <minizarbi@hotmail.fr>
2017-03-17 15:42:01 +0100

    Fix tons of bugs

--- app/src/main/java/com/m2dl/shifoomi/WaitingActivity.java
+++ app/src/main/java/com/m2dl/shifoomi/WaitingActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.m2dl.shifoomi.database.game.Game;
+import com.m2dl.shifoomi.repository.game.GameRepository;
 import com.m2dl.shifoomi.repository.game.GameRepositoryFirebase;
 import com.m2dl.shifoomi.repository.game.GameRepositoryListener;
 import com.m2dl.shifoomi.repository.room.RoomRepository;
@@ -14,11 +15,14 @@ import com.m2dl.shifoomi.repository.room.RoomRepositoryFirebase;
 
 public class WaitingActivity extends AppCompatActivity {
     private String roomId;
+    static boolean isFirst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting);
+
+        isFirst = true;
 
         Intent intent = getIntent();
         roomId = intent.getStringExtra("roomId");
@@ -28,16 +32,22 @@ public class WaitingActivity extends AppCompatActivity {
         GameRepositoryListener listener = new GameRepositoryListener() {
             @Override
             public void gameUpdate(Game game) {
-                Intent intent = new Intent(WaitingActivity.this, GameActivity.class);
-                intent.putExtra("userId", userId);
-                intent.putExtra("gameId", game.getId());
-                startActivity(intent);
+                if (isFirst) {
+                    isFirst = false;
+                    Intent intent = new Intent(WaitingActivity.this, GameActivity.class);
+                    intent.putExtra("userId", userId);
+                    intent.putExtra("gameId", game.getId());
+                    startActivity(intent);
+                }
+
             }
         };
+        GameRepositoryFirebase.getInstance().addGameRepositoryListener(userId, listener);
     }
 
     @Override
     protected void onStop() {
+        super.onStop();
         RoomRepository repository = RoomRepositoryFirebase.getInstance();
         repository.deleteRoom(roomId);
     }
